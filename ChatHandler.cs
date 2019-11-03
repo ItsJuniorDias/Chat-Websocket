b14fb78b6a169f93903cb5aeb6f41baf6cd0b01c
Alexandre <juniordias_@live.com>
2019-11-02 22:27:26 -0300

    connected webSocket

---
+++ ChatHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketManager;
+
+namespace WebSocketChat
+{
+    public class ChatHandler : WebSocketHandler
+    {
+
+        public ChatHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+     
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    var connection = new WebSocketManager.Connection("ws://localhost:5000/chat");
+    connection.enableLogging = true;
+
+    connection.connectionMethods.onConnected = () => {
+
+    }
+
+    connection.connectionMethods.onDisconnected = () => {
+
+    }
+
+    connection.start();
+});
